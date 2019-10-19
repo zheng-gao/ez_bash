324243db2d29a2fdeaf397d22a76f09f6eb3c9ef
Zheng Gao <zgao@linkedin.com>
2019-10-19 02:06:53 -0700

    Minor Fix

--- ez_bash_file/ez_bash_file.sh
+++ ez_bash_file/ez_bash_file.sh
@@ -8,9 +8,11 @@ if [[ "${EZ_BASH_HOME}" == "" ]]; then echo "[EZ-BASH][ERROR] EZ_BASH_HOME is no
 ###################################################################################################
 
 function ez_file_lines() {
-	ez_set_argument --short "-p" --long "--path" --required --info "Path to the file" &&
-    ez_set_argument --short "-f" --long "--from" --default 1 --info "From line" &&
-    ez_set_argument --short "-t" --long "--to" --required --info "To line" || return 1
+    if ! ez_function_exist; then
+	    ez_set_argument --short "-p" --long "--path" --required --info "Path to the file" &&
+        ez_set_argument --short "-f" --long "--from" --default 1 --info "From line" &&
+        ez_set_argument --short "-t" --long "--to" --required --info "To line" || return 1
+    fi
     ez_ask_for_help "${@}" && ez_function_help && return
     local path; path="$(ez_get_argument --short "-p" --long "--path" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
     local from; from="$(ez_get_argument --short "-f" --long "--from" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
