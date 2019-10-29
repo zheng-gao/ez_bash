df9de454e461b3b747ad9691508bf7cc9d923179
Zheng Gao <zgao@linkedin.com>
2019-10-29 11:17:07 -0700

    Simplify get argument usage

--- ez_bash_math/ez_bash_random.sh
+++ ez_bash_math/ez_bash_random.sh
@@ -13,8 +13,8 @@ function ez_get_random_int() {
         ez_set_argument --short "-u" --long "--upper-bound" --required --info "Exclusive Upper Bound" || return 1
     fi
     ez_ask_for_help "${@}" && ez_function_help && return
-    local lower_bound; lower_bound="$(ez_get_argument --short "-l" --long "--lower-bound" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
-    local upper_bound; upper_bound="$(ez_get_argument --short "-u" --long "--upper-bound" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
+    local lower_bound="$(ez_get_argument --short "-l" --long "--lower-bound" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
+    local upper_bound="$(ez_get_argument --short "-u" --long "--upper-bound" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
     [ "${lower_bound}" -gt "${upper_bound}" ] && return 2
     # Use $RANDOM as seed, which is an internal Bash function that returns a pseudo-random integer in the range [0, 32767]
     echo $(( ("${RANDOM}" * 214013 + 2531011) % ("${upper_bound}" - "${lower_bound}") + "${lower_bound}" ))
