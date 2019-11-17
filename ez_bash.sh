f9345bf9ca7578e46c79e8173c6a693b5ea4f24b
Zheng Gao <zgao@linkedin.com>
2019-11-16 18:27:00 -0800

    Rewrite ez_bash

--- ez_bash.sh
+++ ez_bash.sh
@@ -11,17 +11,19 @@
 ###################################################################################################
 # -------------------------------------- Global Variables --------------------------------------- #
 ###################################################################################################
-EZB_LOGO="EZ-BASH"
+EZB_LOGO="EZ-Bash"
 EZB_DIR_WORKSPACE="/var/tmp/ezb_workspace"; mkdir -p "${EZB_DIR_WORKSPACE}"
 EZB_DIR_LOGS="${EZB_DIR_WORKSPACE}/logs"; mkdir -p "${EZB_DIR_LOGS}"
 EZB_DIR_DATA="${EZB_DIR_WORKSPACE}/data"; mkdir -p "${EZB_DIR_DATA}"
+
+EZB_DEFAULT_BASH_VERSION="5"
 EZB_DEFAULT_LOG="${EZB_DIR_LOGS}/ez_bash.log"
 
 ###################################################################################################
 # -------------------------------------- Dependency Check --------------------------------------- #
 ###################################################################################################
 function ezb_command_check() {
-    if ! which "${1}" &> "${EZB_DIR_LOGS}/${FUNCNAME[0]}_${1}.log"; then return 1; else return 0; fi
+    if ! which "${1}" &> "${EZB_DEFAULT_LOG}"; then return 1; else return 0; fi
 }
 
 function ezb_dependency_check() {
@@ -35,36 +37,41 @@ function ezb_dependency_check() {
 ###################################################################################################
 if [[ "${0}" = "-bash" ]] || [[ "${0}" = "-sh" ]]; then
     # To source this script, "${0}" is "-bash" or "-sh"
+    if ! bash --version | grep "version ${EZB_DEFAULT_BASH_VERSION}\." &> "${EZB_DEFAULT_LOG}"; then
+        echo "[${EZB_LOGO}][ERROR] EZ-Bash depends on \"Bash ${EZB_DEFAULT_BASH_VERSION}\" which is not found!"; return 1
+    fi
     [[ -z "${EZ_BASH_HOME}" ]] && echo "\"\${EZ_BASH_HOME}\" is not set!" && return 1
     [[ ! -d "${EZ_BASH_HOME}" ]] && echo "\"${EZ_BASH_HOME}\" is an invalid directory!" && return 1
     # Source Core
-    if ! source "${EZ_BASH_HOME}/ezb/ezb_core.sh"; then echo "Failed to source \"${EZ_BASH_HOME}/ezb/ezb_core.sh\""; return 2; fi
+    if ! source "${EZ_BASH_HOME}/ezb/ezb_core.sh"; then
+        echo "[${EZB_LOGO}][ERROR] Failed to source \"${EZ_BASH_HOME}/ezb/ezb_core.sh\""; return 1
+    fi
     # Source Function
-    if ! ezb_source "${EZ_BASH_HOME}/ezb/ezb_function.sh"; then return 2; fi
+    if ! ezb_source "${EZ_BASH_HOME}/ezb/ezb_function.sh"; then return 1; fi
     # Source Other Libs
     if [[ -z "${1}" ]]; then
         # By default source ALL libs
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_os"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_file"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_math"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_set"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_ssh"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_string"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_time"; then return 2; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_os"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_file"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_math"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_set"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_ssh"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_string"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_time"; then return 1; fi
         # External Lib
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_git"; then return 2; fi
-        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_python"; then return 2; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_git"; then return 1; fi
+        if ! ezb_source_dir --path "${EZ_BASH_HOME}/ezb_python"; then return 1; fi
         echo "[${EZB_LOGO}][INFO] Complete loading EZ-Bash libraries!"
     else
         # Source the designated libraries
-        for ezb_library_name in "${@}"; do if ! ezb_source_dir --path "${EZ_BASH_HOME}/${ezb_library_name}"; then return 2; fi; done
+        for ezb_library_name in "${@}"; do if ! ezb_source_dir --path "${EZ_BASH_HOME}/${ezb_library_name}"; then return 1; fi; done
         unset ezb_library_name
     fi
 else
     # To run this script
     if [[ "$(basename ${0})" = "ez_bash.sh" ]]; then
         if [[ "${1}" = "-i" ]] || [[ "${1}" = "--info" ]]; then echo "EZ-Bash Copyright: Zheng Gao, 2018-05-18"
-        elif [[ "${1}" = "-v" ]] || [[ "${1}" = "--version" ]]; then echo "0.0.5"
+        elif [[ "${1}" = "-v" ]] || [[ "${1}" = "--version" ]]; then echo "0.1.0"
         elif [[ "${1}" = "-r" ]] || [[ "${1}" = "--requirements" ]]; then echo "Bash 5.*"
         fi
     fi
