3e72059f72d1abddd28056c4a934f8d5fa4d5f1e
Zheng Gao <zgao@linkedin.com>
2019-11-19 11:59:48 -0800

    Rewrite ez_bash

--- ezb_time/ezb_time.sh
+++ ezb_time/ezb_time.sh
@@ -7,58 +7,35 @@ function ezb_time_clock() {
     while true; do ez_clear -l 1; ezb_time_now; sleep 1; done
 }
 
-function ezb_epoch_seconds_to_time() {
-    local usage_string=$(ezb_build_usage -o "init" -d "Convert Epoch Seconds to Timestamp")
-    usage_string+=$(ezb_build_usage -o "add" -a "-e|--epoch" -d "Epoch Seconds")
-    usage_string+=$(ezb_build_usage -o "add" -a "-f|--format" -d "Timestamp Format")
-    if [[ "${1}" == "" ]] || [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then ezb_print_usage "${usage_string}"; return 1; fi
-    local epoch_second=""
-    local format="+%Y-%m-%d %H:%M:%S"
-    while [[ ! -z "${1-}" ]]; do
-        case "${1-}" in
-            "-e" | "--epoch") shift; epoch_second=${1-} ;;
-            "-f" | "--format") shift; format=${1-} ;;
-            *)
-                ezb_log_error "Unknown argument \"$1\""
-                ezb_print_usage "${usage_string}"; return 1; ;;
-        esac
-        if [[ ! -z "${1-}" ]]; then shift; fi
-    done
+function ezb_time_from_epoch_seconds() {
+    if ! ezb_function_exist; then
+        ezb_set_arg --short "-e" --long "--epoch" --required --default "0" --info "Epoch Seconds" &&
+        ezb_set_arg --short "-f" --long "--format" --required --default "+%Y-%m-%d %H:%M:%S" --info "Timestamp Format" || return 1
+    fi
+    [[ -n "${@}" ]] && ezb_function_usage "${@}" && return
+    local epoch; epoch="$(ezb_get_arg --short "-e" --long "--epoch" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
+    local format; format="$(ezb_get_arg --short "-f" --long "--format" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
     local os=$(ezb_os_name)
-    if [[ "${os}" == "macos" ]]; then
-        date -r "${epoch_second}" "${format}"
-    elif [[ "${os}" == "linux" ]]; then
-        date "${format}" -d "@${epoch_second}"
+    if [[ "${os}" = "macos" ]]; then date -r "${epoch}" "${format}"
+    elif [[ "${os}" = "linux" ]]; then date "${format}" -d "@${epoch}"
+    else ezb_log_error "Bad ${os}" && return 2
     fi
 }
 
 function ezb_time_to_epoch_seconds() {
-    local usage_string=$(ezb_build_usage -o "init" -d "Convert Human Readable Timestamp to Epoch Seconds")
-    usage_string+=$(ezb_build_usage -o "add" -a "-d|--date" -d "YYYY-MM-DD, default is now")
-    usage_string+=$(ezb_build_usage -o "add" -a "-t|--time" -d "HH:mm:SS, default is now")
-    if [[ "${1}" == "-h" ]] || [[ "${1}" == "--help" ]]; then ezb_print_usage "${usage_string}"; return 1; fi
-    local date=""
-    local time=""
-    while [[ ! -z "${1-}" ]]; do
-        case "${1-}" in
-            "-d" | "--date") shift; date=${1-} ;;
-            "-t" | "--time") shift; time=${1-} ;;
-            *)
-                ezb_log_error "Unknown argument \"$1\""
-                ezb_print_usage "${usage_string}"; return 1; ;;
-        esac
-        if [[ ! -z "${1-}" ]]; then shift; fi
-    done
-    if [[ "${date}" == "" ]] && [[ "${time}" == "" ]]; then date "+%s"
-    elif [[ "${date}" == "" ]]; then ezb_log_error "Date cannot be empty if Time is given"; ezb_print_usage "${usage_string}"; return 1
-    elif [[ "${time}" == "" ]]; then ezb_log_error "Time cannot be empty if Date is given"; ezb_print_usage "${usage_string}"; return 1
-    else
-        local os=$(ezb_os_name)
-        if [[ "${os}" == "macos" ]]; then
-            date -j -f "%Y-%m-%d %H:%M:%S" "${date} ${time}" "+%s"
-        elif [[ "${os}" == "linux" ]]; then
-            date -d "${date} ${time}" "+%s"
-        fi
+    if ! ezb_function_exist; then
+        ezb_set_arg --short "-d" --long "--date" --required --default "Today" --info "YYYY-MM-DD" &&
+        ezb_set_arg --short "-t" --long "--time" --required --default "Now" --info "HH:mm:SS" || return 1
+    fi
+    [[ -n "${@}" ]] && ezb_function_usage "${@}" && return
+    local date; date="$(ezb_get_arg --short "-d" --long "--date" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
+    local time; time="$(ezb_get_arg --short "-t" --long "--time" --arguments "${@}")"; [ "${?}" -ne 0 ] && return 1
+    [[ "${date}" = "Today" ]] && date=$(date "+%F")
+    [[ "${time}" = "Now" ]] && time=$(date "+%H:%M:%S")
+    local os=$(ezb_os_name)
+    if [[ "${os}" = "macos" ]]; then date -j -f "%Y-%m-%d %H:%M:%S" "${date} ${time}" "+%s"
+    elif [[ "${os}" = "linux" ]]; then date -d "${date} ${time}" "+%s"
+    else ezb_log_error "Bad ${os}" && return 2
     fi
 }
 
