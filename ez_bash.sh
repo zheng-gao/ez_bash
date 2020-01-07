567fd3a7038d3a3661c5180b7aab70d781d4f0f9
Zheng Gao <zgao@linkedin.com>
2020-01-07 12:28:07 -0800

    Fix ez_bash.sh

--- ez_bash.sh
+++ ez_bash.sh
@@ -18,7 +18,25 @@ EZB_DEFAULT_BASH_VERSION="5"
 ###################################################################################################
 # ---------------------------------------- Main Function ---------------------------------------- #
 ###################################################################################################
-if [[ "${0}" = "-bash" ]] || [[ "${0}" = "-sh" ]]; then
+if [[ "${0:0:1}" != "-" ]] && [[ "$(basename ${0})" = "ez_bash.sh" ]]; then
+    # To run this script
+    if [[ -z "${1}" ]] || [[ "${1}" = "-h" ]] || [[ "${1}" = "--help" ]]; then
+        echo; echo "[Usage]"
+        echo "  -i|--info              Show Copyright"
+        echo "  -v|--version           Show Version"
+        echo "  -r|--requirements      Show Requirements"; echo
+        echo "To import EZ-Bash libraries: \"source ${EZ_BASH_HOME}/ez_bash.sh\""; echo
+    fi
+    while [[ -n "${1}" ]]; do
+        case "${1}" in
+            "-i" | "--info") shift; echo "EZ-Bash Copyright: Zheng Gao, 2018-05-18" ;;
+            "-v" | "--version") shift; echo "${EZB_VERSION}" ;;
+            "-r" | "--requirements") shift; echo "Bash ${EZB_DEFAULT_BASH_VERSION}" ;;
+            *) echo "[EZ-Bash][ERROR] Unknown argument identifier \"${1}\""; exit 1 ;;
+        esac
+        [[ -n "${1}" ]] && shift
+    done
+else
     # To source this script, "${0}" is "-bash" or "-sh"
     bash --version | grep "version ${EZB_DEFAULT_BASH_VERSION}\." &> "/var/tmp/null" || {
         echo "[EZ-Bash][ERROR] \"Bash ${EZB_DEFAULT_BASH_VERSION}\" not found!"; return 1
@@ -51,25 +69,5 @@ if [[ "${0}" = "-bash" ]] || [[ "${0}" = "-sh" ]]; then
             ezb_source_dir --path "${EZ_BASH_HOME}/${ezb_library_name}" || return 1
         done
         unset ezb_library_name
-    fi
-else
-    # To run this script
-    if [[ "$(basename ${0})" = "ez_bash.sh" ]]; then
-        if [[ -z "${1}" ]] || [[ "${1}" = "-h" ]] || [[ "${1}" = "--help" ]]; then
-            echo; echo "[Usage]"
-            echo "  -i|--info              Show Copyright"
-            echo "  -v|--version           Show Version"
-            echo "  -r|--requirements      Show Requirements"; echo
-            echo "To import EZ-Bash libraries: \"source ${EZ_BASH_HOME}/ez_bash.sh\""; echo
-        fi
-        while [[ -n "${1}" ]]; do
-            case "${1}" in
-                "-i" | "--info") shift; echo "EZ-Bash Copyright: Zheng Gao, 2018-05-18" ;;
-                "-v" | "--version") shift; echo "${EZB_VERSION}" ;;
-                "-r" | "--requirements") shift; echo "Bash ${EZB_DEFAULT_BASH_VERSION}" ;;
-                *) echo "[EZ-Bash][ERROR] Unknown argument identifier \"${1}\""; exit 1 ;;
-            esac
-            [[ -n "${1}" ]] && shift
-        done
     fi
 fi
