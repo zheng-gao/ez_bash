6c7dd51d4cf341caf04b504bef7880c5467f2e36
Zheng Gao <zgao@evaav.com>
2023-09-29 13:19:32 -0700

    Add ez_show_fingerprint

--- src/libs/ssl.sh
+++ src/libs/ssl.sh
@@ -18,3 +18,16 @@ function ez_show_cert {
     local port && port="$(ez_arg_get --short "-p" --long "--port" --arguments "${@}")" || return 1
     echo | openssl "s_client" -connect "${hostname}:${port}" -showcerts | openssl "x509" -noout -text
 }
+
+
+function ez_show_fingerprint {
+    if ez_function_unregistered; then
+        ez_arg_set --short "-c" --long "--cert" --required --info "File path to the cert" &&
+        ez_arg_set --short "-s" --long "--sha" --required --default "SHA1" --choices "SHA1" "SHA256" --info "SHA type" ||
+        return 1
+    fi
+    ez_function_usage "${@}" && return
+    local cert && cert="$(ez_arg_get --short "-c" --long "--cert" --arguments "${@}")" &&
+    local sha && sha="$(ez_arg_get --short "-s" --long "--sha" --arguments "${@}")" || return 1
+    openssl "x509" -noout -fingerprint "-${sha}" -in "${cert}"
+}
